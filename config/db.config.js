da6a3956a609c20c9f3dc08879ec459a63ed0b55
ihsanpratamaaa <ihsanpratamaaa@gmail.com>
2024-09-03 11:38:44 +0700

    latest update for code_name

--- config/db.config.js
+++ config/db.config.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
-module.exports = {
-  HOST: process.env.DB_HOST || "34.124.207.224",
-  PORT: process.env.DB_PORT ||"5432",
-  USER: process.env.DB_USER ||"Nero-Admin",
-  PASSWORD: process.env.DB_PASSWORD ||"Neroadmin12",
-  DB: process.env.DB_DBNAME ||"Nero"
-};
-
 // require('dotenv').config();
 // module.exports = {
-//   HOST: process.env.DB_HOST || "localhost",
-//   PORT: process.env.DB_PORT || "5432",
-//   USER: process.env.DB_USER ||"postgres",
-//   PASSWORD: process.env.DB_PASSWORD || "admin",
-//   DB: process.env.DB_DBNAME || "geco"
+//   HOST: process.env.DB_HOST || "34.124.207.224",
+//   PORT: process.env.DB_PORT ||"5432",
+//   USER: process.env.DB_USER ||"Nero-Admin",
+//   PASSWORD: process.env.DB_PASSWORD ||"Neroadmin12",
+//   DB: process.env.DB_DBNAME ||"Nero"
 // };
+
+require('dotenv').config();
+module.exports = {
+  HOST: process.env.DB_HOST || "localhost",
+  PORT: process.env.DB_PORT || "5432",
+  USER: process.env.DB_USER ||"postgres",
+  PASSWORD: process.env.DB_PASSWORD || "admin",
+  DB: process.env.DB_DBNAME || "geco"
+};
