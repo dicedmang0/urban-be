9f426811479c7799fcaa82fd3a07f71f4afa3f7c
ihsanpratamaaa <mggibran@gmail.com>
2024-10-09 14:01:54 +0700

    UPDATING FOR CORS

--- app.js
+++ app.js
@@ -26,15 +26,13 @@ const app = express();
 // Configure CORS options
 const corsOptions = {
   origin: [
+    'http://127.0.0.1:3001',
     'https://urban-gaming.com',
     'https://advance-genre-425305-g1.web.app',
-    'https://urban-umber-alpha.vercel.app',
-    'http://127.0.0.1:3001'
-
+    'https://urban-umber-alpha.vercel.app'
   ], // Replace with your desired origin & add FE URL
   methods: 'GET,PUT,POST,DELETE',
-  allowedHeaders: ['Content-Type', 'X-Access-Token'],
-  credentials: true
+  allowedHeaders: ['Content-Type', 'X-Access-Token']
 };
 
 // Use CORS middleware with options
