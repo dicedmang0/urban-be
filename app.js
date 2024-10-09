4583cf52df5e880c39e2a7aa184a67fff84e281e
ihsanpratamaaa <mggibran@gmail.com>
2024-10-09 15:28:06 +0700

    UPDATING FOR CORS ()

--- app.js
+++ app.js
@@ -47,6 +47,16 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use(bodyParser.json());
 
 router(app)
